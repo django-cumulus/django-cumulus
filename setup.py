afed16845a8216b2bfc5ca4c0326d2b529d15c36
Rich Leland <rich@richleland.com>
2009-07-28 23:31:00 -0400

    Added setup script and related files.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from distutils.core import setup
+
+f = open(os.path.join(os.path.dirname(__file__), 'README'))
+long_description = f.read().strip()
+f.close()
+
+setup(
+    name='django-cumulus',
+    version='0.1',
+    description='A Django app for working with Rackspace Cloud APIs.',
+    long_description=long_description,
+    author='Rich Leland',
+    author_email='rich@richleland.com',
+    url='https://bitbucket.org/richleland/django-cumulus/',
+    download_url='https://bitbucket.org/richleland/django-cumulus/downloads/',
+    packages=['cumulus', 'cumulus.templates'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
