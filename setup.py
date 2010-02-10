8814149757a58dd2188a4bc440b51a2f037ad0bc
Rich Leland <rich@richleland.com>
2010-02-09 21:00:58 -0500

    Prepped setup files for 0.2 release.

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-import os
-from distutils.core import setup
+import os, distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup, find_packages
 
-f = open(os.path.join(os.path.dirname(__file__), 'README'))
+f = open(os.path.join(os.path.dirname(__file__), 'docs/index.rst'))
 long_description = f.read().strip()
 f.close()
 
 setup(
-    name='django-cumulus',
-    version='0.1',
-    description='A Django app for working with Rackspace Cloud APIs.',
-    long_description=long_description,
-    author='Rich Leland',
-    author_email='rich@richleland.com',
-    url='https://bitbucket.org/richleland/django-cumulus/',
-    download_url='https://bitbucket.org/richleland/django-cumulus/downloads/',
-    packages=['cumulus'],
+    name = "django-cumulus",
+    version = "0.2",
+    packages = find_packages(),
+    
+    author = "Rich Leland",
+    author_email = "rich@richleland.com",
+    description = "An interface to Rackspace Cloud Files through Django.",
+    long_description = long_description,
+    url = "http://bitbucket.org/richleland/django-cumulus/",
+    download_url = "http://bitbucket.org/richleland/django-cumulus/get/0.2.tar.gz",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
