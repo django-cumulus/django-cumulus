dfc9cda1f738b9849e48991df0ad70bb22698bb0
Rich Leland <rich@richleland.com>
2012-01-29 20:32:26 -0500

    Created example project based on Django 1.3.

---
+++ example/photos/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Photo(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='photos')
+
+    def __unicode__(self):
+        return self.title
---
+++ example/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.conf.urls.defaults import patterns, include, url
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+)
