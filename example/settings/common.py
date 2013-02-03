420b8bb0a8bd0db5a7f3e23cb816baa691d4be02
rizumu <tom@rizu.mu>
2013-02-03 16:09:42 +0100

    fix project root in test settings

--- example/settings/common.py
+++ example/settings/common.py
@@ -1,6 +1,6 @@
 import os
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
