62caf671e06d75f04eb86d10aa468f551877e559
Ferrix Hovi <ferrix@ferrix.fi>
2013-05-18 11:03:41 +0300

    Bump version to 1.1
    * Maintenance branch for 1.x (non-swiftclient) versions
    * Retry sending on connection failure

--- cumulus/__init__.py
+++ cumulus/__init__.py
@@ -3,8 +3,8 @@ An interface to Rackspace Cloud Files through Django.
 """
 __version_info__ = {
     'major': 1,
-    'minor': 0,
-    'micro': 10,
+    'minor': 1,
+    'micro': 0,
     'releaselevel': 'final',
     'serial': 1
 }
