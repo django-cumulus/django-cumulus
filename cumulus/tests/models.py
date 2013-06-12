3d4421a6857888dbc662ebb9ad4d8b4498006596
Michel Sabchuk <michel@turbosys.com.br>
2013-06-12 18:00:34 -0300

    Tests for gzip content.

--- cumulus/tests/models.py
+++ cumulus/tests/models.py
@@ -7,6 +7,8 @@ openstack_storage = SwiftclientStorage()
 
 class Thing(models.Model):
     "A dummy model to use for tests."
-    image = models.ImageField(storage=openstack_storage, upload_to="cumulus-tests")
+    image = models.ImageField(storage=openstack_storage,
+                              upload_to="cumulus-tests",
+                              blank=True)
     document = models.FileField(storage=openstack_storage, upload_to="cumulus-tests")
     custom = models.FileField(storage=openstack_storage, upload_to="cumulus-tests")
