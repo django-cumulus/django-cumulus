7e7f06b19dff50f58a82d18c80217e37d13d2c6f
Michel Sabchuk <michel@turbosys.com.br>
2013-06-13 12:45:55 -0300

    The read method gets the whole file if no chunk_size specified.

--- cumulus/storage.py
+++ cumulus/storage.py
@@ -329,10 +329,17 @@ class SwiftclientStorageFile(File):
 
     file = property(_get_file, _set_file)
 
-    def read(self, chunk_size):
-        if self._pos == self._get_size():
+    def read(self, chunk_size=-1):
+        """
+        Reads specified chunk_size or the whole file if chunk_size is None.
+        """
+        if self._pos == self._get_size() or chunk_size == 0:
             return ""
-        data = self.file.get(chunk_size=chunk_size).next()
+
+        if chunk_size < 0:
+            data = self.file.get()
+        else:
+            data = self.file.get(chunk_size=chunk_size).next()
         self._pos += len(data)
         return data
 
