1b9e8ec9c61b9153166bdab7fa6ee4cfa5a2712d
Thomas Schreiber <tom@rizu.fake>
2013-12-13 16:09:50 +0100

    a simpler manage.py

--- example/manage.py
+++ example/manage.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
-import imp
-try:
-    imp.find_module("settings")  # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write(
-        "Error: Can't find the file 'settings.py' in the directory containing"
-        "{0}. It appears you've customized things.\nYou'll have to run"
-        "django-admin.py, passing it your settings module.\n".format(__file__))
-    sys.exit(1)
-
-from django.core.management import execute_manager
-
-import settings
+import os
+import sys
 
 
 if __name__ == "__main__":
-    execute_manager(settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
