991c67f422705d507ed4d720436e1dde7ad5738d
Ferrix Hovi <ferrix.hovi@recruitby.net>
2014-09-02 01:04:16 +0300

    Fix error from readthedocs.org
        /latest/docs/index.rst:385: WARNING: Definition list ends without
        a blank line; unexpected unindent.

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,10 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.append(os.path.abspath('.'))
+import os
+import sys
+
+sys.path.append(os.path.abspath('..'))
 
 import cumulus
 
