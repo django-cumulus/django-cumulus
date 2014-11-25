86faa64ff1f0a82dcb108bb4838da43bc0a9fc95
Ferrix Hovi <ferrix@ferrix.fi>
2014-11-25 18:52:10 +0000

    User versioneer for versioning
    This is exactly what we want for version numbering and this is it
    on a silver plate. Yay.

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 import os
+import versioneer
 
 from setuptools import setup, find_packages
 
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'cumulus/_version.py'
+versioneer.versionfile_build = None
+versioneer.tag_prefix = ''
+versioneer.parentdir_prefix = 'django-cumulus-'
 
 setup(
-    name="django-cumulus",
-    version=__import__("cumulus").get_version().replace(" ", "-"),
-    packages=find_packages(),
-    install_requires=[
+    name = "django-cumulus",
+    version = versioneer.get_version(),
+    cmdclass = versioneer.get_cmdclass(),
+    packages = find_packages(),
+    install_requires = [
         "pyrax>=1.9,<1.10",
     ],
-    author="Ferrix Hovi, Thomas Schreiber",
-    license="BSD",
-    description="An interface to python-swiftclient and rackspace cloudfiles API from Django.",
-    long_description=open("README.rst").read(),
-    url="https://github.com/django-cumulus/django-cumulus/",
-    classifiers=[
+    author = "Ferrix Hovi, Thomas Schreiber",
+    license = "BSD",
+    description = "An interface to python-swiftclient and rackspace cloudfiles API from Django.",
+    long_description = open("README.rst").read(),
+    url = "https://github.com/django-cumulus/django-cumulus/",
+    classifiers = [
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
