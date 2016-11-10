68739c1482965137de0989184351c0514e2214c4
Yi Ming Yung <yi.ming.yung@wearespindle.com>
2016-11-10 10:40:55 +0100

    Add fail_silently option in Auth class.

--- cumulus/authentication.py
+++ cumulus/authentication.py
@@ -22,6 +22,7 @@ class Auth(object):
     auth_tenant_name = CUMULUS["AUTH_TENANT_NAME"]
     auth_version = CUMULUS["AUTH_VERSION"]
     pyrax_identity_type = CUMULUS["PYRAX_IDENTITY_TYPE"]
+    fail_silently = CUMULUS.get('FAIL_SILENTLY', True)
 
     def __init__(self, username=None, api_key=None, container=None,
                  connection_kwargs=None, container_uri=None):
@@ -49,11 +50,13 @@ class Auth(object):
             self.pyrax.set_setting("region", self.region)
             try:
                 self.pyrax.set_credentials(self.username, self.api_key)
-            except:
+            except Exception as e:
                 logging.exception(
                     """Pyrax Connect Error in `django_cumulus.cumulus.authentication.Auth`::
                            self.pyrax.set_credentials(self.username, self.api_key)
                     """)
+                if not self.fail_silently:
+                    raise e
         # else:
         #     headers = {"X-Container-Read": ".r:*"}
         #     self._connection.post_container(self.container_name, headers=headers)
